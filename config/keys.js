1003974953fb6524c2f3b696a256a9bd2473a6ad
Vilu <vehnia.ville@gmail.com>
2020-01-19 16:37:11 +0200

    Use another database

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://new-user_1:MobileAppDev@node-app-5otnv.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://new-user_1:MobileAppDev@node-app-5otnv.mongodb.net/mobile-app?retryWrites=true&w=majority",
     secretOrKey: "secret"
 }
